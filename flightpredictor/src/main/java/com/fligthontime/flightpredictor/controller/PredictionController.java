94fe8779eb87b54d62d28345d1f740340eb2de97
Faner <fanerstder@gmail.com>
2026-01-23 23:49:30 -0500

    Imprememtacion de la lista de predicionesCacche

--- flightpredictor/src/main/java/com/fligthontime/flightpredictor/controller/PredictionController.java
+++ flightpredictor/src/main/java/com/fligthontime/flightpredictor/controller/PredictionController.java
@@ -1,11 +1,14 @@
 package com.fligthontime.flightpredictor.controller;
 
+import com.fligthontime.flightpredictor.dto.PredictionListResponse;
 import com.fligthontime.flightpredictor.dto.PredictionRequest;
 import com.fligthontime.flightpredictor.dto.PredictionResponse;
 import com.fligthontime.flightpredictor.service.LocalModelService;
 import com.fligthontime.flightpredictor.service.PredictionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -24,6 +27,11 @@ public class PredictionController {
     ) {
         // Delegamos TODA la lógica al servicio principal, pasando el modo
         return predictionService.predict(request, mode);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PredictionListResponse>> listPredictions(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(predictionService.listPredictions(page, size));
     }
 
     @GetMapping("/debug-model")
--- flightpredictor/src/main/java/com/fligthontime/flightpredictor/service/PredictionService.java
+++ flightpredictor/src/main/java/com/fligthontime/flightpredictor/service/PredictionService.java
@@ -1,6 +1,7 @@
 package com.fligthontime.flightpredictor.service;
 
 import com.fligthontime.flightpredictor.dto.EnrichedData;
+import com.fligthontime.flightpredictor.dto.PredictionListResponse;
 import com.fligthontime.flightpredictor.dto.PredictionRequest;
 import com.fligthontime.flightpredictor.dto.PredictionResponse;
 import com.fligthontime.flightpredictor.entity.PredictionCache;
@@ -8,6 +9,9 @@ import com.fligthontime.flightpredictor.repository.PredictionRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -87,5 +91,11 @@ public class PredictionService {
         }
 
         return response;
+    }
+
+    public Page<PredictionListResponse> listPredictions(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<PredictionCache> predictions = repository.findAll(pageable);
+        return predictions.map(PredictionListResponse::new);
     }
 }
